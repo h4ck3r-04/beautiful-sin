1d07ac1bcdf4c91c9ea490ade6eff642e853c3ca
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-06-21 10:24:48 +0530

    fixed: ui bug

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
+let content = document.querySelector(".content")
 
 btn.onclick = function () {
   sidebar.classList.toggle("active");
+  content.classList.toggle("active");
 }
