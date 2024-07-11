871ed7214785f3e1ab70262c8d9b52de4dcd900a
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-07-11 13:20:51 +0530

    created base design for cyberchef

--- app/misc/page.tsx
+++ app/misc/page.tsx
@@ -1,10 +1,11 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card"
+import CyberChef from "@/pages/CyberChef"
 
 export default function Misc() {
     return (
         <div className="min-h-screen">
-            <Tabs defaultValue="cyberchef" className="space-y-4">
+            <Tabs defaultValue="cyberchef" className="space-y-4 w-[113%]">
                 {/* Tabs */}
                 <TabsList>
                     <TabsTrigger value="cyberchef">
@@ -13,9 +14,8 @@ export default function Misc() {
                 </TabsList>
 
                 {/* Tabs Content */}
-                <TabsContent value="cyberchef" className="space-y-4">
-                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                    </div>
+                <TabsContent value="cyberchef" className="space-y-4 border-[1px] border-white rounded-xl">
+                    <CyberChef />
                 </TabsContent>
             </Tabs>
         </div>
