40d10058e152ff63f2e4da229eaa60f817a37cea
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-07-29 14:32:39 +0530

    added: web-check config

---
+++ api/web-check/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"cmp"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Host          string
+	Port          string
+	AllowedOrigin string
+}
+
+func New() Config {
+	host := getEnvDefault("HOST", "localhost")
+	port := getEnvDefault("PORT", "8080")
+	return Config{
+		Host:          host,
+		Port:          port,
+		AllowedOrigin: getEnvDefault("ALLOWED_ORIGINS", fmt.Sprintf("http://%s:%s", host, port)),
+	}
+}
+
+func getEnvDefault(key, def string) string {
+	return cmp.Or(os.Getenv(key), def)
+}
