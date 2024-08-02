22f8944a4b4568becac8fdf4ec437a77eeb9f02f
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-08-02 13:42:20 +0530

    added: draggable components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ import DynamicComponentLoader from "@/props/DynamicContentLoader";
 
 const Dashboard: React.FC = () => {
   const [selectedComponents, setSelectedComponents] = useState<string[]>([]);
+  const [draggedComponentIndex, setDraggedComponentIndex] = useState<
+    number | null
+  >(null);
 
   const handleSelectComponent = (componentName: string) => {
     setSelectedComponents((prevComponents) => [
@@ -14,12 +17,36 @@ const Dashboard: React.FC = () => {
     ]);
   };
 
+  const handleDragStart = (index: number) => {
+    setDraggedComponentIndex(index);
+  };
+
+  const handleDrop = (index: number) => {
+    if (draggedComponentIndex !== null) {
+      const updatedComponents = [...selectedComponents];
+      const [draggedComponent] = updatedComponents.splice(
+        draggedComponentIndex,
+        1,
+      );
+      updatedComponents.splice(index, 0, draggedComponent);
+      setSelectedComponents(updatedComponents);
+      setDraggedComponentIndex(null);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <Search onSelect={handleSelectComponent} />
       <div className="flex flex-wrap justify-center items-start mt-4 mx-auto">
         {selectedComponents.map((componentName, index) => (
-          <div key={index} className="p-2">
+          <div
+            key={index}
+            className="p-2"
+            draggable
+            onDragStart={() => handleDragStart(index)}
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={() => handleDrop(index)}
+          >
             <DynamicComponentLoader componentName={componentName} />
           </div>
         ))}
